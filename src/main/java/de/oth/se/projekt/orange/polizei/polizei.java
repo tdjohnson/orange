50172623e2529e36a1a058d5193532d27c4ef975
StudiUsi <StudiUsi@Usi-Gamerlaptop.fritz.box>
2015-10-27 14:07:46 +0100

    changed package name of "polizei"

--- src/main/java/de/oth/se/projekt/orange/polizei/polizei.java
+++ src/main/java/de/oth/se/projekt/orange/polizei/polizei.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.oth.projekt.orange.polizei;
+package de.oth.se.projekt.orange.polizei;
 
 /**
  *
