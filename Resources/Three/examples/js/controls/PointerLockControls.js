ff706c8e786e620c86598835048e633800c0a97a
Derusi <usi.usinger@gmail.com>
2015-12-25 19:12:55 +0100

    normalizing vectors

--- Resources/Three/examples/js/controls/PointerLockControls.js
+++ Resources/Three/examples/js/controls/PointerLockControls.js
@@ -21,8 +21,8 @@ THREE.PointerLockControls = function ( camera ) {
 
 		if ( scope.enabled === false ) return;
 
-		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+		var movementX = event.movementX || event.mozMovementX || event.movementX || 0;
+		var movementY = event.movementY || event.mozMovementY || event.movementY || 0;
 
 		yawObject.rotation.y -= movementX * 0.002;
 		pitchObject.rotation.x -= movementY * 0.002;
