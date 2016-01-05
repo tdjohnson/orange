52a0a190c428015827a414d8cd7be83a9985b73b
Derusi <usi.usinger@gmail.com>
2016-01-05 17:23:53 +0100

    fixing floor

--- draft/main.js
+++ draft/main.js
@@ -154,10 +154,10 @@ function showinfo(intersect){
 function collisionDetection() {
 	for (var i=0; i<collidableMeshList.length; i++) {
 		var bbox = collidableMeshList[i];
-		if ((controls.getObject().position.x+0.3 >= bbox.box.min.x) &&
-			(controls.getObject().position.x+0.3 <= bbox.box.max.x) &&
-			(controls.getObject().position.z+0.3 >= bbox.box.min.z) &&
-			(controls.getObject().position.z+0.3 <= bbox.box.max.z))
+		if ((controls.getObject().position.x+0.3 >= toilet.box.min.x) &&
+			(controls.getObject().position.x+0.3 <= toilet.box.max.x) &&
+			(controls.getObject().position.z+0.3 >= toilet.box.min.z) &&
+			(controls.getObject().position.z+0.3 <= toilet.box.max.z))
 			 {
 			 	//console.log(bbox.box.min.x+" "+bbox.box.max.x+" "+bbox.box.min.y+" "+bbox.box.max.y);
 				return false;
